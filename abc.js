6ef3caab3a4e526c912d7471770cc43bf8b2fe4c
Pooja <poojadahiya121001@gmail.com>
2022-06-17 20:07:02 +0530

    first commit

---
+++ abc.js
@@ -0,0 +1,16 @@
+
+var i =parseInt(process.argv[2]);
+for( let i=1; i<= 10;  i++){
+    console.log(i);
+}
+var i =parseInt(process.argv[2])
+
+for( let i=1; i<= 20;  i++){
+    if(i%2==0)
+    console.log(i);
+}
+var i =parseInt(process.argv[2])
+for( let i=20; i>= 1;  i--){
+    console.log(i);
+} 
+
