96d78044bd6922961abd2d3ddcd69210d678eab1
Pooja <poojadahiya121001@gmail.com>
2022-06-17 20:17:58 +0530

    add

--- abc.js
+++ abc.js
@@ -13,4 +13,23 @@ var i =parseInt(process.argv[2])
 for( let i=20; i>= 1;  i--){
     console.log(i);
 } 
-
+var i =parseInt(process.argv[2]);
+var i=1;
+while( i<= 10){
+console.log(i);
+i++
+}
+var i =parseInt(process.argv[2]);
+var i=1;
+while( i<= 20){
+    if(i%2==0)
+console.log(i);
+i++
+}
+var i =parseInt(process.argv[2]);
+var i=20;
+while( i>= 1){
+    
+console.log(i);
+i--
+}
