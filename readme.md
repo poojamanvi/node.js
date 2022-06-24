190f711c07419ea2709b6d3aff94d34d9122c033
Pooja <poojadahiya121001@gmail.com>
2022-06-24 15:55:06 +0530

    add

--- readme.md
+++ readme.md
@@ -6,3 +6,10 @@
 <img src="Capture_4.PNG" alt="img">
 <img src="Capture_5.PNG" alt="img">
 <img src="Capture_6.PNG" alt="img">
+<img src="img_1.PNG" alt="img">
+<img src="img_2.PNG" alt="img">
+<img src="img_3.PNG" alt="img">
+<img src="img_4.PNG" alt="img">
+<img src="img_5.PNG" alt="img">
+<!-- <img src="img_6.PNG" alt="img">
+<img src="img_6.PNG" alt="img"> -->
--- recurrison_Assignment_question.js
+++ recurrison_Assignment_question.js
@@ -1,25 +1,61 @@
-// function x(num,facto){
-//     if(num>0){
-//         facto = facto*num;
-//         return x((num-1),facto);
+// Assignment Question
+
+// Question_1
+// print all number in the given range
+
+// function range(a,b){
+//     if(a<=b){
+//         console.log(a)
+//         return range((a+1),b)
 //     }
-//     return facto;
-
 // }
-// console.log(x(5,1))
+// range(1,10)
+
+// Question_2
+// Print all the numbers in descending order in the given range 
+
+// function descendingOrder(a,b){
+//     if(b>=a)
+//     console.log(b)
+//     return descendingOrder(a,b-1)
+// }
+// descendingOrder(1,10)
 
 
-// function x(num1,num2,counter){
+// Question_3
+// print all the even number in the given range
+
+// function evenNumber(a,b){
+//     if(a<=b && a%2==0){
+//         console.log(a)
+//         return evenNumber(a+2,b)
+//     }
     
-//     if(counter<10){
-        
-//         let c= num1+num2  
-          
-//         return x(num2,c,counter+1)
-//     }
-//     console.log(num1);
-       
-
 // }
-// x(1,1,1)
+// evenNumber(10,20)
 
+// Question_4
+// find the factorial of the given number
+
+ 
+function factorial(num,facto) {
+  
+    if(num>0){
+        facto=facto*num
+        return factorial(num-1,facto)
+    }
+   console.log(facto) 
+}
+factorial(10,1)
+
+// Question_5
+// find the nth digit of fibonacci series
+
+function fibonacci(num1,num2,count){
+    if(count<=8){
+        let c=num1+num2
+        return fibonacci(num2,c,count+1)
+    }
+    console.log(num2);
+}
+fibonacci(20,30,1)
